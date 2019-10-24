a40e7a236f8537391ce269b4f6134a318baf4367
Asiful Choudhury <asiful307@gmail.com>
2019-10-24 20:02:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,10 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-    
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliette", age: "15", status: "alive"},
+      :heroine_friends => []
    }
   }
 
